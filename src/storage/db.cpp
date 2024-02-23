c86df26f8db4ab63ce7830eeb7763b5056ba5b4f
NilEra <88582666+NilEra-K@users.noreply.github.com>
2024-02-23 19:12:21 +0800

    Add files via upload

--- src/storage/db.cpp
+++ src/storage/db.cpp
@@ -87,29 +87,85 @@ int db_c::get(const char* appid, const char* userid, const char* fileid, std::st
     // 将文件ID和路径及大小的对应关系保存在缓存中
     value.format("%lld;%s", *filesize, filepath.c_str());
     cache.set(key, value.c_str());
-    
-
     return OK;
 }
 
 // 设置 文件ID和路径 及 大小 的对应关系
 int db_c::set(const char* appid, const char* userid, const char* fileid, const char* filepath, long long filesize) const {
+    // 根据用户ID获取其对应的表名
+    std::string tablename = table_of_user(userid);
+    if (tablename.empty()) {
+        logger_error("Tabel Name is Empty, Appid: %s, Userid: %s, Fileid: %s", appid, userid, fileid);
+        return ERROR;
+    }
+
+    // 插入一条记录
+    acl::string sql;
+    sql.format("INSERT INTO %s SET id='%s', appid='%s', userid='%s', status=0, file_path=%s, file_size=%lld;", tablename.c_str(), fileid, appid, userid, filepath, filesize);
+    if (mysql_query(m_mysql, sql.c_str())) {
+        logger_error("INSERT Database Fail: %s, SQL: %s", mysql_error(m_mysql), sql.c_str());
+        return ERROR;
+    }
+
+    // 检查插入结果
+    MYSQL_RES* res = mysql_store_result(m_mysql);
+    if (!res && mysql_field_count(m_mysql)) {
+        logger_error("INSERT Database Fail: %s, SQL: %s", mysql_error(m_mysql), sql.c_str());
+        return ERROR;
+    }
+
     return OK;
 }
 
 // 删除文件ID
 int db_c::del(const char* appid, const char* userid, const char* fileid) const {
+    // 先从缓存中删除文件ID
+    cache_c cache;
+    acl::string key;
+    key.format("uid:fid:%s:%s", userid, fileid);
+    if (cache.del(key) != OK) {
+        logger_warn("DELETE Cache Fail, Appid: %s, Userid: %s, Fileid: %s", appid, userid, fileid);
+    }
+
+    // 再从数据库中删除文件ID
+    std::string tablename = table_of_user(userid);
+    if (tablename.empty()) {
+        logger_error("Tabel Name is Empty, Appid: %s, Userid: %s, Fileid: %s", appid, userid, fileid);
+        return ERROR;
+    }
+
+    acl::string sql;
+    sql.format("DELETE FROM %s WHERE id='%s';", tablename.c_str(), fileid);
+    if (mysql_query(m_mysql, sql.c_str())) {
+        logger_error("DELETE Database Fail: %s, SQL: %s", mysql_error(m_mysql), sql.c_str());
+        return ERROR;
+    }
+
+    // 检查删除结果
+    MYSQL_RES* res = mysql_store_result(m_mysql);
+    if (!res && mysql_field_count(m_mysql)) {
+        logger_error("DELETE Database Fail: %s, SQL: %s", mysql_error(m_mysql), sql.c_str());
+        return ERROR;
+    }
     return OK;
 }
 
 // 根据用户ID获取其对应的表名
 std::string db_c::table_of_user(const char* userid) const {
-    return "";
+    char tablename[10];     // Table Name: t_file_xx\0
+    sprintf(tablename, "t_file_%02d", (hash(userid, strlen(userid) & 0x7FFFFFFF)) % 3 + 1);
+    return tablename;
 }
 
 // 计算哈希值
 unsigned int db_c::hash(const char* buf, size_t len) const {
-    return 0;
+    unsigned int h = 0;
+    for (size_t i = 0; i < len; ++i) {
+        // h = h ^ xxx
+        // i&1 判断奇偶
+        h ^= i&1 ? ~(h<<11 ^ buf[i] ^ h>>5) : h << 7 ^ buf[i] ^ h>>3;
+    }
+    return h;
 }
 
 // 析构函数
