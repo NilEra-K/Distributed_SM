39c7a3e42b54ff857bb8df448ff1cb361896935b
NilEra <88582666+NilEra-K@users.noreply.github.com>
2024-02-26 17:39:53 +0800

    Add files via upload

--- src/storage/id.cpp
+++ src/storage/id.cpp
@@ -50,7 +50,7 @@ long id_c::client(const char* requ, long long requlen) const {
     int nrand = rand() % nids;
     for (int i = 0; i < nids; ++i) {
         if (conn.open(g_iaddrs[nrand].c_str(), 0, 0)) {
-            logger("Connect ID SUCCESS, Addr: %s", g_iaddrs);
+            logger("Connect ID SUCCESS, Addr: %s", g_iaddrs[nrand].c_str());
             break;
         } else {
             logger("Connect ID FAIL, Addr: %s", g_iaddrs[nrand].c_str());
@@ -63,5 +63,26 @@ long id_c::client(const char* requ, long long requlen) const {
         return -1;
     }
 
-    return -1;
+    // 向ID服务器发送请求
+    if (conn.write(requ, requlen) < 0) {
+        logger_error("Write Fail: %s, Requlen: %lld, To: %s", acl::last_serror(), requlen, conn.get_peer());
+        conn.close();
+        return -1;
+    }
+
+    // 从ID服务器接收响应
+    long long resplen = HEADLEN + BODYLEN_SIZE;
+    char resp[resplen] = {};
+    if (conn.read(resp, resplen) < 0) {
+        logger_error("Read Fail: %s, Requlen: %lld, FROM: %s", acl::last_serror(), requlen, conn.get_peer());
+        conn.close();
+        return -1;
+    }
+
+    // | 包体长度 | 命令 | 状态 | ID  值 |
+    // |    8   |   1  |  1  | 包体长度|
+    // 从 ID服务器的响应中解析ID值
+    long value = ntol(resp + HEADLEN);
+    conn.close();
+    return value;
 }
